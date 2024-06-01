10c8e581f7b20a4bd12db43f04b73a2cb3205492
Viktor Panaiot <vpanaet@outlook.com>
2024-06-01 13:55:31 +0300

    set up SQl Connection. create SnippetModel

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"lets-go-snippetbox/internal/models"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "DSN to create database connection")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
@@ -20,12 +27,36 @@ func main() {
 		Level:     slog.LevelDebug,
 	}))
 
-	app := application{logger: logger}
+	db, err := openDb(*dsn)
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	app := application{logger: logger, snippets: &models.SnippetModel{DB: db}}
 
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
