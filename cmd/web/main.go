f3274a13a7332292230a848ff884d67304c42239
Viktor Panaiot <vpanaet@outlook.com>
2024-06-19 09:14:04 +0300

    use http.Server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -58,9 +58,15 @@ func main() {
 
 	app := application{logger: logger, snippets: &models.SnippetModel{DB: db}, templateCache: templateCache, formDecoder: formDecoder, sessionManager: sessionManager}
 
+	srv := http.Server{
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err = http.ListenAndServe(*addr, app.routes())
+	err = srv.ListenAndServe()
 
 	logger.Error(err.Error())
 	os.Exit(1)
