fb6c3940bd0ebd35d03451aa1404ebcaa3d2b916
Viktor Panaiot <vpanaet@outlook.com>
2024-06-28 22:56:08 +0300

    add csrf protection

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/justinas/nosurf"
 	"net/http"
 )
+
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+
+	return csrfHandler
+}
 
 func commonHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
